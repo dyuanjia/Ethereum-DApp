f8e2c17a9f8bdaffbf822191e999f8c3145c36fd
dyuanjia <yuanjia_dong@mymail.sutd.edu.sg>
2020-04-22 21:40:17 +0800

    completed contract

--- src/contracts/BlindAuction.sol
+++ src/contracts/BlindAuction.sol
@@ -1,19 +1,29 @@
 pragma solidity ^0.6.1;
 
 contract BlindAuction {
+    // uint256 => 256 bits
+    // 8 bytes nonce => 64 bits
+    // max bid => 192 bits
+    uint256 constant public MAX_BID = 2**192 - 1;
+    bool ended = false;
+    
     string public name;
     string public description;
-    address public owner;
+    address payable public owner;
     uint256 public minimumBid;
     uint256 public biddingEndTime;
     uint256 public revealEndTime;
-    uint256 stageDuration = 2 hours;
+    //uint256 stageDuration = 2 hours;
+    uint256 stageDuration = 10; // for testing
     
     struct BlindBid {
         bytes32 hash;
         uint secret;
     }
     mapping (address => BlindBid) bids;
+    // Allowed withdrawals of previous bids
+    mapping (address => uint) pendingReturns;
+    address payable public highestBidder;
     
     modifier onlyBefore(uint _time, address _sender) { 
         require(now < _time);
@@ -28,16 +38,18 @@ contract BlindAuction {
     constructor(
         string memory _name,
         string memory _description,
-        address _owner,
+        address payable _owner,
         uint256 _minimum_bid) public {
             require(bytes(_name).length > 0, "name cannot be empty");
             require(bytes(_description).length > 0, "description cannot be empty");
+            require(_minimum_bid < MAX_BID, "minimum bid cannot be higher than max bid amount");
             name = _name;
             description = _description;
             owner = _owner;
             minimumBid = _minimum_bid;
             biddingEndTime = now + stageDuration;
             revealEndTime = biddingEndTime + stageDuration;
+            highestBidder = _owner;
     }
     
     function bid(bytes32 _hash) external onlyBefore(biddingEndTime, msg.sender) {
@@ -46,21 +58,68 @@ contract BlindAuction {
         emit Bid(msg.sender, _hash);
     }
     
+    function getBid() external view returns(bytes32) {
+        return bids[msg.sender].hash;
+    }
+    
     function withdraw() external onlyBefore(biddingEndTime, msg.sender) {
         delete bids[msg.sender];
         emit Bid(msg.sender, 0);
     }
-
     
-    function calculateHash(uint secret) public pure returns(bytes32){
-        bytes memory b = new bytes(32);
-        assembly { mstore(add(b, 32), secret) }
-        return sha256(b);
+    function reveal(bytes8 _nonce) payable external 
+        onlyBefore(revealEndTime, msg.sender)
+        onlyAfter(biddingEndTime) {
+            // checks
+            require(bids[msg.sender].hash != 0, "you must have bidded during the bidding stage");
+            require(msg.value > minimumBid, "your bid is not high enough");
+            require(msg.value < MAX_BID, "bid exceeds max bid amount");
+            require(bids[msg.sender].secret == 0, "bid already revealed");
+            bytes32 hash = calculateHash(msg.value, _nonce);
+            require(hash == bids[msg.sender].hash, "either bid amount or nonce is incorrect");
+            // effects
+            uint256 refund = bids[highestBidder].secret;
+            address payable prevHighestBidder = highestBidder;
+            minimumBid = msg.value;
+            bids[msg.sender].secret = msg.value;
+            highestBidder = msg.sender;
+            // interactions
+            emit Reveal(msg.sender, msg.value);
+            if (prevHighestBidder != owner) {
+                prevHighestBidder.transfer(refund);
+            }
+    }
+    
+    function endAuction() external onlyAfter(revealEndTime) {
+        require(ended == false, "auction already ended");
+        ended = true;
+        if (highestBidder != owner) {
+            emit AuctionEnded(highestBidder, minimumBid);
+            owner.transfer(minimumBid);
+        } else {
+            emit AuctionEnded(owner, 0);
+        }
+    }
+    
+    function calculateHash(uint256 _secret, bytes8 _nonce) public pure returns(bytes32){
+        require(_secret < MAX_BID, "bid exceeds max bid amount");
+        bytes32 concat = bytes32(_secret << 64 | uint64(_nonce));
+        return sha256(abi.encodePacked(concat));
     }
     
     event Bid(
         address indexed _bidder,
         bytes32 _hash
+    );
+    
+    event Reveal(
+        address indexed _bidder,
+        uint256 _value
+    );
+    
+    event AuctionEnded(
+        address indexed _bidder,
+        uint256 _value
     );
 
 }
--- test/BlindAuction.test.js
+++ test/BlindAuction.test.js
@@ -10,7 +10,14 @@ contract(
       "You've heard the legend of the Diamond. A meteorite that was found by Genghis Khan. Carried at the head of his armies. Owned by murderous Kings, and tragic Queens. It may sound like a campfire ghost-story. Curses, legends... Madness, death.";
     // 0.01 ether
     const minBid = "10000000000000000";
-    const stageDuration = 60 * 60 * 2;
+    const stageDuration = 10;
+    // Sample bids
+    const secret1 = "1000000000000000000";
+    const nonce1 = "0xdeadbeefdeadbeef";
+    const hash1 =
+      "0x65d912dee76723f0b8e3363c0f11ebf449ff0c197fef2d411eb16c5e25cb31dc";
+    // Invalid variables
+    const tooHighBid = (2 ** 219).toString();
 
     before(async () => {
       //developer deploys the contract
@@ -42,6 +49,10 @@ contract(
           parseInt(_revealEndTime, 10) - parseInt(_biddingEndTime, 10);
         assert.equal(_duration, stageDuration);
       });
+      it("set the owner as the default highest bidder", async () => {
+        const _highestBidder = await blindAuction.highestBidder();
+        assert.equal(_highestBidder, itemOwner);
+      });
     });
 
     describe("Invalid constructor", async () => {
@@ -56,9 +67,59 @@ contract(
           .should.be.rejected;
       });
       it("will never have a negative minimum bid", async () => {
-        const testAuction = await BlindAuction.new(name, desc, itemOwner, "-1");
-        const _minBid = await testAuction.minimumBid();
-        assert.isAtLeast(parseInt(_minBid, 10), 0);
+        await BlindAuction.new(name, desc, itemOwner, "-1").should.be.rejected;
+      });
+      it("must have minimum bid lower than the maximum bid amount", async () => {
+        await BlindAuction.new(name, desc, itemOwner, tooHighBid).should.be
+          .rejected;
+      });
+    });
+
+    describe("Function calculateHash(uint256 _secret, bytes8 _nonce)", async () => {
+      it("calculates the hash correctly", async () => {
+        const _hash = await blindAuction.calculateHash(secret1, nonce1);
+        assert.equal(_hash, hash1);
+      });
+      it("must have a value lower than the maximum bid amount", async () => {
+        await blindAuction.calculateHash(tooHighBid, nonce1).should.be.rejected;
+      });
+      it("must have a valid 8 byte nonce", async () => {
+        await blindAuction.calculateHash(secret1, "invalid").should.be.rejected;
+      });
+    });
+
+    describe("Bidding Stage", async () => {
+      it("prevents owner from bidding", async () => {
+        await blindAuction.bid(hash1, { from: itemOwner }).should.be.rejected;
+      });
+      it("allows a bidder to bid and emits a Bid event", async () => {
+        const response = await blindAuction.bid(hash1, {
+          from: bidder1,
+        });
+        const event = response.logs[0].args;
+        assert.equal(event._bidder, bidder1);
+        assert.equal(event._hash, hash1);
+      });
+      it("must bid with a valid hash", async () => {
+        await blindAuction.bid("invalid", {
+          from: bidder1,
+        }).should.be.rejected;
+      });
+      it("allows a bidder to retrieve his bid with getBid()", async () => {
+        const bid = await blindAuction.getBid({ from: bidder1 });
+        assert.equal(bid, hash1);
+      });
+      it("allows a bidder to withdraw, i.e. set his bid to 0", async () => {
+        const response = await blindAuction.withdraw({ from: bidder1 });
+        const event = response.logs[0].args;
+        assert.equal(event._bidder, bidder1);
+        assert.equal(event._hash, 0);
+      });
+      it("prevents a bidder to reveal in this stage", async () => {
+        await blindAuction.reveal(nonce1, { from: bidder1 }).should.be.rejected;
+      });
+      it("prevents anyone from ending the auction in this stage", async () => {
+        await blindAuction.endAuction({ from: bidder1 }).should.be.rejected;
       });
     });
   }
